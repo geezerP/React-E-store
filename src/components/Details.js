57373ba5c4c4c15b9934b8fcd00f74e4abf86dd3
geezerP <mwaurameshack64@gmail.com>
2019-07-30 10:44:16 +0300

    Modified Details.js

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,7 +8,7 @@ export default class Details extends Component{
 		return(
 			<ProductConsumer>
 				{value =>{
-					const{id,company,img,info,price,title,inCart}=value.detailProduct;
+					const{id,company,img,info,price,title,inCart}=value.storeProduct;
 					return(
 						<div className="container py-5">
 							{/* title */}
