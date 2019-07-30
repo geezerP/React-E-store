d7948d651f00b821df1421b24efcf670ec62e361
geezerP <mwaurameshack64@gmail.com>
2019-07-30 11:05:50 +0300

    Changed details.js to correct routing error

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,7 +8,7 @@ export default class Details extends Component{
 		return(
 			<ProductConsumer>
 				{value =>{
-					const{id,company,img,info,price,title,inCart}=value.storeProducts;
+					const{id,company,img,info,price,title,inCart}=value.detailProduct;
 					return(
 						<div className="container py-5">
 							{/* title */}
